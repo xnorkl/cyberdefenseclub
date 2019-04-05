bea32ea936ba15505afb75393159f34e0bc56174
xnor <root@lilith.localdomain>
2019-04-05 12:34:33 -0400

    refactored udpserver.py

--- udpserver.py
+++ udpserver.py
@@ -1,25 +1,66 @@
-import socket
 import base64
-# pick a port to accept UDP datagrams and define a buffer size
-port = 8081
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# bind the port
-s.bind(("", port))
-reply = "waiting on port: {0}".format(port)
-print(reply)
+import socket
+from enum import Enum
 
-while 1:
-    # accept up to 1024 bytes per datagram
-    data, addr = s.recvfrom(1024)
-    # decode the message and store it as a string
+p = 8081
+m = 1024
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+class Verbosity(Enum):
+    NONE = 0
+    SOME = 1
+    FULL = 2
+
+def prompt(port):
+    reply = "Receiving prompt on port {0}".format(port) 
+    return reply
+
+def bind_udp_port(port,maxbytes):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(("",port))
+    datagram = s.recvfrom(maxbytes)
+    return datagram
+    
+def get_networkinfo(gram):
+    # find network info from a message
+    netinfo = gram[1]
+    return netinfo
+
+def decrypt_data(data):
+    # take a byte, then decrypt & return a string
     msg = str(base64.b64decode(data))
-    #hex = ":".join("{:02x}".format(ord(c)) for c in msg)
-    # parse the address
+    return msg
+
+def format_message(datagram,verbosity):   
+    # type checking
+    if not isinstance(verbosity, Verbosity):
+        raise TypeError('verbosity must be an instance of Verbosity')
+
+    # flatten datagram and decrypt data
+    data, addr = datagram
     ip, port = addr
-    # make our answer readable
-    msg = str(msg)
-    msg = msg[1:].strip("'")
-    ip.strip("'")
-    answer = "Received {0} from {1}:{2}".format(msg,ip,port)
-    print(answer)
+    ip = ip.strip("'")
+    msg = decrypt_data(data)
+    text = msg[1:].strip("'") 
+    if verbosity.value == 1:
+        message = "{0}:{1} > {2}".format(ip,port,text)
+    elif verbosity.value == 2:
+        message = "{0}:{1} > {2}\n{3}".format(ip,port,text,data)
+    else:
+        message = "> {0}".format(text)
+    return message
+
+def gethex_data(msg):
+    # take string and return the hex value 
+    hex = ":".join("{:02x}".format(ord(c)) for c in msg)
+    return hex
+
+# use this for debugging for now
+while 1:
+    dgram = bind_udp_port(p,m)
+    print(prompt(p))
+    print(format_message(dgram, Verbosity.FULL))
+    print(format_message(dgram, Verbosity.SOME))
+    print(format_message(dgram, Verbosity.NONE))
+
 
